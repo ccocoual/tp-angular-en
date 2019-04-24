a2f067ed3611fe5c1d3b98fd8b0f49a4335f30f1
Corentin Cocoual <corentin.cocoual@zenika.com>
2019-04-23 20:43:46 -0400

    ✅ Running unit tests and fix broke ones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tp-angular-en'`, () => {
+  it(`should have as title 'my App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tp-angular-en');
+    expect(app.title).toEqual('my App');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tp-angular-en!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my App!');
   });
 });
