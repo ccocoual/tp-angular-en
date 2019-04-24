2b215505612ddf522dd50cff8ddd40f3761e0ed6
Corentin Cocoual <corentin.cocoual@zenika.com>
2019-04-23 21:57:23 -0400

    ✅ Test new components and fix app component tests

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ProductComponent } from './product.component';
+import { ProductComponent } from "./product.component";
+import { Product } from "../model/product";
 
-describe('ProductComponent', () => {
+const testProduct = new Product("title", "description", "photo", 42);
+
+describe("ProductComponent", () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.data = testProduct;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should bind title and price in the h3", () => {
+    const h3Content = fixture.nativeElement.querySelector("h3").textContent;
+    expect(h3Content).toContain(testProduct.title);
+    expect(h3Content).toContain(testProduct.price);
+  });
+
+  it("should bind the photo url", () => {
+    const img = fixture.nativeElement.querySelector("img");
+    expect(img.src).toContain(testProduct.photo);
+  });
+
+  it("should emit addToBasket event on a click on the button", () => {
+    spyOn(component.addToBasket, "emit");
+    const button = fixture.nativeElement.querySelector("button");
+    button.click();
+    expect(component.addToBasket.emit).toHaveBeenCalled();
   });
 });
