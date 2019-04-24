9ad7029683d84708b52a3033cb99a46372db1e0a
Corentin Cocoual <corentin.cocoual@zenika.com>
2019-04-23 23:06:06 -0400

    ✅ Adds test for components that uses new services

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,49 +3,71 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { Product } from './model/product';
+import { CustomerService } from './services/customer.service';
+import { ProductService } from './services/product.service';
+
+const testProducts = [new Product('', '', '', 0, 0), new Product('', '', '', 0, 0)];
+const fakeAppTitle = 'FAKE APP TITLE';
+
+class ProductServiceMock {
+  getProducts() {
+    return testProducts;
+  }
+  isAvailable() {
+    return true;
+  }
+  decreaseStock() {}
+}
+
+class CustomerServiceMock {
+  getTotal() {
+    return 42;
+  }
+  addProduct() {}
+}
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let customerService: CustomerService;
+  let productService: ProductService;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [
+        { provide: ProductService, useClass: ProductServiceMock },
+        { provide: CustomerService, useClass: CustomerServiceMock },
+        { provide: 'appTitle', useValue: fakeAppTitle },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-  }));
+    customerService = TestBed.get(CustomerService);
+    productService = TestBed.get(ProductService);
+  });
 
-  it('should create the app', async(() => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  });
 
-  it('should have a total starting at 0', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.total).toEqual(0);
-  }));
-
-  it('should have the total bound in the header', async(() => {
+  it('should have the title bound in the header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     const compiled = fixture.debugElement.nativeElement;
 
-    app.total = 42;
     fixture.detectChanges();
-    expect(compiled.querySelector('header').textContent).toContain(42);
-  }));
+    expect(compiled.querySelector('header').textContent).toContain(fakeAppTitle);
+  });
 
-  it('should update price with the product price', async(() => {
+  it('should have the total bound in the header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    const compiled = fixture.debugElement.nativeElement;
 
-    const product = new Product('', '', '', 666, 2);
-    app.total = 42;
-    app.updatePrice(product);
-    expect(app.total).toBe(42 + 666);
-    expect(product.stock).toBe(1);
-  }));
+    fixture.detectChanges();
+    expect(compiled.querySelector('header').textContent).toContain(customerService.getTotal());
+  });
 
-  it('should bind each product component with its product', async(() => {
+  it('should bind each product component with its product', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     const compiled = fixture.debugElement.nativeElement;
@@ -55,17 +77,36 @@ describe('AppComponent', () => {
     products.forEach((product, i) => {
       expect(product.data).toBe(app.products[i]);
     });
-  }));
+  });
 
-  it('should not display product with an empty stock', async(() => {
+  it('should call addProduct and decreaseStock when updatePrice', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const product = testProducts[0];
+
+    spyOn(customerService, 'addProduct');
+    spyOn(productService, 'decreaseStock');
+
+    app.updatePrice(product);
+    expect(customerService.addProduct).toHaveBeenCalledWith(product);
+    expect(productService.decreaseStock).toHaveBeenCalledWith(product);
+  });
+
+  it('should not display product which is not available', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     const compiled = fixture.debugElement.nativeElement;
 
-    app.products = [new Product('empty', '', '', 42, 0), new Product('available', '', '', 42, 10)];
+    spyOn(productService, 'isAvailable').and.callFake(product => {
+      if (product === testProducts[0]) {
+        return false;
+      }
+      return true;
+    });
+
     fixture.detectChanges();
     const products = compiled.querySelectorAll('app-product');
     expect(products.length).toBe(1);
     expect(products[0].data).toBe(app.products[1]);
-  }));
+  });
 });
--- src/app/services/customer.service.spec.ts
+++ src/app/services/customer.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CustomerService } from './customer.service';
+import { Product } from '../model/product';
+
+const product1 = new Product('', '', '', 42, 0);
+const product2 = new Product('', '', '', 666, 0);
 
 describe('CustomerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CustomerService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CustomerService],
+    });
+    service = TestBed.get(CustomerService);
+  });
 
-  it('should be created', () => {
-    const service: CustomerService = TestBed.get(CustomerService);
+  it('should be created with no product', () => {
     expect(service).toBeTruthy();
+    expect(service.products.length).toBe(0);
+  });
+
+  it('should add products to the list when using addProduct', () => {
+    service.addProduct(product1);
+    expect(service.products).toEqual([product1]);
+  });
+
+  it('should calculate the total price when using getTotal', () => {
+    service.products = [product1, product2];
+    expect(service.getTotal()).toBe(product1.price + product2.price);
   });
 });
