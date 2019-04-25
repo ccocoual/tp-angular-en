9a64d0d327d2aee46702f109eb0144a232908501
Corentin Cocoual <corentin.cocoual@zenika.com>
2019-04-24 20:58:45 -0400

    ✨ Use pipes to format data and create a sorting pipe for products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import localeEnCa from '@angular/common/locales/en-CA';
+registerLocaleData(localeEnCa, 'en-CA');
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProductComponent } from './product/product.component';
+import { SortPipe } from './pipes/sort.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, ProductComponent],
+  declarations: [AppComponent, MenuComponent, ProductComponent, SortPipe],
   imports: [BrowserModule],
   providers: [
     {
       provide: 'appTitle',
       useValue: 'Welcome to Zenika E-Shop',
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'en-CA',
     },
   ],
   bootstrap: [AppComponent],
