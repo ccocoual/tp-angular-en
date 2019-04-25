b792e9f4f61ff76e16ca90fe477c6e2d439f18da
Corentin Cocoual <corentin.cocoual@zenika.com>
2019-04-24 20:59:53 -0400

    ✨ Pass property to sort onto as pipe parameter

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -6,12 +6,12 @@ import { Product } from '../model/product';
   name: 'sort',
 })
 export class SortPipe implements PipeTransform {
-  transform(value: Product[]): Product[] {
+  transform(value: Product[], column: keyof Product = 'title'): Product[] {
     return [...value].sort((p1, p2) => {
-      if (p1.title > p2.title) {
+      if (p1[column] > p2[column]) {
         return 1;
       }
-      if (p1.title < p2.title) {
+      if (p1[column] < p2[column]) {
         return -1;
       }
       return 0;
